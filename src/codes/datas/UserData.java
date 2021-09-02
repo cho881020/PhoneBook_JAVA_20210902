9163896c1d393429b34af7bd0b305a5d208967c7
조경진 <cho881020@gmail.com>
2021-09-02 14:45:16 +0900

    UserData의 toString 오버라이딩.
      - 한국 나이 계산 몌소드 추가
      - Calendar 클래스 이용, 올해 몇년인지 받아오기

--- src/codes/datas/UserData.java
+++ src/codes/datas/UserData.java
@@ -1,10 +1,32 @@
 package codes.datas;
 
+import java.util.Calendar;
+
 public class UserData {
 	
 	private String name;
 	private String phoneNum;
 	private int birthYear;
+	
+	
+	public int getKoreanAge() {
+		
+//		올해의 나이가 몇살인가?
+//		올해 : 지금 일시를 불러내서 -> 그 중 연도값만 추출.
+		int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+		
+		return  thisYear - this.birthYear + 1;
+		
+	}
+	
+//	toString메쏘드 오버라이딩.
+	@Override
+	public String toString() {
+
+		return String.format("%s(%d세) - %s", this.name, this.getKoreanAge(), this.phoneNum);
+		
+	}
+	
 	
 //	생성자 커스터마이징. 모든 데이터를 한번에 세팅.
 	public UserData(String name, String phoneNum, int birthYear) {
